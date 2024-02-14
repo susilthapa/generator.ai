df4ac963149ed2f02c287f49879d35b734447733
Susil Thapa <thapasusil53@gmail.com>
2024-02-14 19:30:14 +0545

    utils to get video and generate transcripts

---
+++ src/lib/youtube.ts
@@ -0,0 +1,45 @@
+import { YoutubeTranscript } from "youtube-transcript";
+import { strict_output } from "./gpt";
+
+export async function getTranscript(videoId: string) {
+  try {
+    let transcript_arr = await YoutubeTranscript.fetchTranscript(videoId, {
+      lang: "en",
+      country: "EN",
+    });
+    const transcript_strings = transcript_arr.reduce(
+      (preVal, transcript) => preVal + transcript.text + " ",
+      ""
+    );
+    return transcript_strings.replaceAll("\n", "");
+  } catch (error) {
+    return "";
+  }
+}
+
+export async function getQuestionsFromTranscript(
+  transcript: string,
+  course_title: string
+) {
+  type Question = {
+    question: string;
+    answer: string;
+    option1: string;
+    option2: string;
+    option3: string;
+  };
+  const questions: Question[] = await strict_output(
+    "You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not be more than 15 words",
+    new Array(5).fill(
+      `You are to generate a random hard mcq question about ${course_title} with context of the following transcript: ${transcript}`
+    ),
+    {
+      question: "question",
+      answer: "answer with max length of 15 words",
+      option1: "option1 with max length of 15 words",
+      option2: "option2 with max length of 15 words",
+      option3: "option3 with max length of 15 words",
+    }
+  );
+  return questions;
+}
